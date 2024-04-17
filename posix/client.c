6aada341dbbcf1a445d22926b654620f5fa5c3f4
Glorison <lai.glorison@gmail.com>
2024-04-17 11:06:51 +1000

    add posix implementation

---
+++ posix/client.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+
+void error(const char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+int main(int argc, char *argv[]) {
+  int sockfd, portno;
+  int err;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+
+  char buffer[255];
+  if (argc < 3) {
+    fprintf(stderr, "Usage: %s hostname port\n", argv[0]);
+    exit(1);
+  }
+
+  portno = atoi(argv[2]);
+  sockfd = socket(PF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    error ("Error opening socket");
+  }
+
+  server = gethostbyname(argv[1]);
+  if (server == NULL) {
+    fprintf(stderr, "Error: no such host");
+    exit(1);
+  }
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+  serv_addr.sin_port = htons(portno);
+
+  err = connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+  if (err < 0) {
+    error("Could not connect to server");
+  }
+
+  while (1) {
+    bzero(buffer, 255);
+    fgets(buffer, 255, stdin);
+    err = write(sockfd, buffer, strlen(buffer));
+    if (err < 0) {
+      error("Error on Write");
+    }
+    bzero(buffer, 255);
+    err = read(sockfd, buffer, 255);
+    if (err < 0) {
+      error("Error on Read");
+    }
+
+    int i = strcmp("Bye", buffer);
+    if (i == 0) {
+      break;
+    }
+  }
+
+  close(sockfd);
+  return 0;
+}
+
---
+++ posix/server.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+
+int const MAX_BACKLOG = 5;
+
+void error(const char *msg) {
+  perror(msg);
+  exit(1);
+}
+
+int main(int argc, char *argv[1]) {
+  if (argc < 2) {
+    fprintf(stderr, "Port No not provided. Program terminated\n");
+  }
+
+  int sockfd, newsockfd, portno;
+  char buffer[255];
+  int err;
+
+  struct sockaddr_in serv_addr, cli_addr;
+  socklen_t clilen;
+
+  // Beej Programming Guide
+  // Address families were thought to maybe support several protocols, which didn't end up happening.
+  // Effectively same thing (sys/socket.h:20)
+  // PF_INET in socket call
+  // AF_INET in struct sockaddr_in
+  // beej.us/guide/bgnet/html/#system-calls-or-bust
+  sockfd = socket(PF_INET, SOCK_STREAM, 0);
+
+  if (sockfd < 0) {
+    error("Could not allocate socket");
+  }
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  portno = atoi(argv[1]);
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+
+  if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    error("Binding failed.");
+  }
+
+  err = listen(sockfd, MAX_BACKLOG);
+
+  if (err < 0) {
+    error("Listening failed.");
+  } else {
+    printf("Listening on %d:\n", portno);
+  }
+
+  clilen = sizeof(cli_addr);
+
+  newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
+
+  if (newsockfd < 0) {
+    error("Error on Accept");
+  } else {
+    printf("Accepted Client!\n");
+  }
+  
+  while (1) {
+    bzero(buffer, 255);
+    err = read(newsockfd, buffer, 255);
+
+    if (err < 0) {
+      error("Error on Read");
+    }
+    printf("Client : %s\n", buffer);
+    bzero(buffer, 255);
+    fgets(buffer, 255, stdin);
+
+    err = write(newsockfd, buffer, strlen(buffer));
+    if (err < 0) {
+      error("Error on Write");
+    }
+
+    int i = strcmp("Bye", buffer);
+    if (i == 0) {
+      break;
+    }
+  }
+
+  close(newsockfd);
+  close(sockfd);
+
+  return 0;
+}
